2055f1a5c76fd4047fbe93923f73c46106752e6b
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-03-29 16:49:08 -0300

    Introdução, Abstração e Encapsulamento com C#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ExemploPOO.Models;
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Marco";
+// p1.Idade = 59;
+
+// p1.Apresentar();
+
+ContaCorrente cc = new ContaCorrente(123, 1000);
+
+cc.ExibirSaldo();
+cc.Sacar(5000);
+cc.ExibirSaldo();
